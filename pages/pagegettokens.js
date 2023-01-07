3b9ee78d82d01faf3ade6b5ac303cf1a5d968f96
PornHubMaster <life@live.be>
2023-01-07 18:43:58 +0100

    bug_correction

--- pages/pagegettokens.js
+++ pages/pagegettokens.js
@@ -1,23 +1,48 @@
-import { useState } from 'react';
+import React, { useState } from "react";
+import { ethers } from "../node_modules/ethers/dist/ethers.esm.js";
 
-function PageGetTokens() {
-    const [number, setNumber] = useState('');
-    
-    function handleChange(event) {
-        setNumber(event.target.value);
-    }
+const ConnectPage = () => {
+    const [connected, setConnected] = useState(false);
+    const [addressUser, setAddressUser] = useState("");
+    const [provider, setProvider] = useState(null);
+    const [signer, setSigner] = useState(null);
+
+    const connect = async () => {
+        setProvider(new ethers.providers.Web3Provider(window.ethereum));
+        await provider.send("eth_requestAccounts", []);
+        setSigner(provider.getSigner());
+        setAddressUser(await signer.getAddress());
+        setConnected(true);
+    };
+
+    const invest = async () => {
+        const amount = document.querySelector(".amount").value;
+        const transactionRequest = {
+            to: "0xDC18a7dc0823593A8e060ee177a78AE30d864834", //platform Goerli test address
+            from: addressUser,
+            value: ethers.utils.parseEther(amount)
+        };
+
+        await signer.sendTransaction(transactionRequest);
+
+    };
 
     return (
         <div>
-            <button class="connectButton">Connect your wallet</button>
-            <form>
-                <label>
-                    <p>Amount of ETH to invest:</p>
-                    <input type="text" value={number} onChange={handleChange} />
-                </label>
-            </form>
+            {!connected && (
+                <button className="connectButton" onClick={connect}>
+                    Connect
+                </button>
+            )}
+            {connected && (
+                <div className="connected">Connected with: {addressUser}</div>
+            )}
+            <input className="amount" />
+            <button className="invest" onClick={invest}>
+                Invest
+            </button>
         </div>
     );
-}
+};
 
-export default PageGetTokens;
+export default ConnectPage;
