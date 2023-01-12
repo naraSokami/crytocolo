2543f872133fdf244905ef2e2d62d86d4474aad0
naraSokami <derwaeldaltonbaptiste@gmail.com>
2023-01-12 16:38:00 +0100

    add three scroll code

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 const path = require('path')
 
+const withTM = require('next-transpile-modules')(['three'])
+module.exports = withTM()
+
 module.exports = {
   sassOptions: {
     includePaths: [
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,101 @@
 import styles from '../styles/pages/welcome.module.sass';
+import * as THREE from "three";
+import React, { useRef, useEffect } from "react";
+import { Canvas, useFrame } from "react-three-fiber";
+import { Block, useBlock } from "../src/three/blocks";
+import state from "../src/three/store";
 
-export default function Page() {
+function Plane({ color = "white", ...props }) {
   return (
-    <div className={styles.welcome}>
-      <h1>Because we <span>care</span> <br />About <span>future</span></h1>
-      {/* <div className="glow" style={{ top: '-30px', left: '-70px' }}></div> */}
-    </div>
+    <mesh {...props}>
+      <planeGeometry />
+      <meshBasicMaterial color={color} />
+    </mesh>
+  );
+}
+
+function Cross() {
+  const ref = useRef();
+  const { viewportHeight } = useBlock();
+  useFrame(() => {
+    const curTop = state.top.current;
+    const curY = ref.current.rotation.z;
+    const nextY =
+      (curTop / ((state.pages - 1) * viewportHeight * state.zoom)) * Math.PI;
+    ref.current.rotation.z = THREE.MathUtils.lerp(curY, nextY, 0.1);
+  });
+  return (
+    <group ref={ref} scale={[2, 2, 2]}>
+      <Plane scale={[1, 0.2, 0.2]} color="#e2bfca" />
+      <Plane scale={[0.2, 1, 0.2]} color="#e2bfca" />
+    </group>
+  );
+}
+
+function Content({ left, children }) {
+  const { contentMaxWidth, canvasWidth, margin } = useBlock();
+  const aspect = 1.75;
+  const alignRight = (canvasWidth - contentMaxWidth - margin) / 2;
+  return (
+    <group position={[alignRight * (left ? -1 : 1), 0, 0]}>
+      <Plane
+        scale={[contentMaxWidth, contentMaxWidth / aspect, 1]}
+        color="#bfe2ca"
+      />
+      {children}
+    </group>
+  );
+}
+
+function Stripe() {
+  const { contentMaxWidth } = useBlock();
+  return (
+    <Plane
+      scale={[100, contentMaxWidth, 1]}
+      rotation={[0, 0, Math.PI / 4]}
+      position={[0, 0, -1]}
+      color="#e3f6f5"
+    />
+  );
+}
+
+
+export default function () {
+  const scrollArea = useRef();
+  const onScroll = (e) => (state.top.current = e.target.scrollTop);
+  useEffect(() => void onScroll({ target: scrollArea.current }), []);
+
+  return (
+    <>
+      <Canvas
+        linear
+        orthographic
+        camera={{ zoom: state.zoom, position: [0, 0, 500] }}
+      >
+        {/* First section */}
+        <Block factor={1.5} offset={0}>
+          <Content left />
+        </Block>
+        {/* Second section */}
+        <Block factor={2.0} offset={1}>
+          <Content />
+        </Block>
+        {/* Stripe */}
+        <Block factor={-1.0} offset={1}>
+          <Stripe />
+        </Block>
+        {/* Last section */}
+        <Block factor={1.5} offset={2}>
+          <Content left>
+            <Block factor={-0.5}>
+              <Cross />
+            </Block>
+          </Content>
+        </Block>
+      </Canvas>
+      <div className="scrollArea" ref={scrollArea} onScroll={onScroll}>
+        <div style={{ height: `${state.pages * 100}vh` }} />
+      </div>
+    </>
   )
 }
